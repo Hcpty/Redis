d7a1fdb12a317deee0400d1faa63d77cde0cf42c
Hcpty <hcpty@outlook.com>
2025-03-02 19:33:50 +0800

    Update set_up_redis.sh

--- set_up_redis.sh
+++ set_up_redis.sh
@@ -1,18 +1,20 @@
 set -eux
 
-# Install Redis
+echo "Install Redis"
 pkg install databases/redis
 
-# Configure Redis
+echo "Configure Redis"
 sed -i -e 's/bind 127.0.0.1 -::1/# bind 127.0.0.1 -::1/' /usr/local/etc/redis.conf
 sed -i -e 's/# aclfile \/etc\/redis\/users.acl/aclfile \/etc\/redis\/users.acl/' /usr/local/etc/redis.conf
 mkdir /usr/local/etc/redis/
 touch /usr/local/etc/redis/users.acl
+
+echo "Configure Redis users"
 echo 'user default on '$PASSWORD' ~* &* +@all' >> /usr/local/etc/redis/users.acl
 echo 'user server on '$PASSWORD' ~* &* +@all' >> /usr/local/etc/redis/users.acl
 
-# Enable auto start
+echo "Enable auto start"
 sysrc redis_enable="YES"
 
-# Start
+echo "Start Redis"
 service redis start
